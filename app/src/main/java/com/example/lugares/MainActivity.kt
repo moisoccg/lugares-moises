bf6ec610759bf66393729718932625cb9b1c9373
andres.navarrete <andres.navarrete@ibm.com>
2022-06-01 00:24:08 -0600

    Adding the activityMainBinding

--- app/src/main/java/com/example/lugares/MainActivity.kt
+++ app/src/main/java/com/example/lugares/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.lugares
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lugares.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initializing firebase
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
     }
 }
