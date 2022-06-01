0b6eda7305f9007b4ce5b5aff1f42d482fea08f5
andres.navarrete <andres.navarrete@ibm.com>
2022-06-01 01:09:39 -0600

    first class complete

--- app/src/main/java/com/example/lugares/MainActivity.kt
+++ app/src/main/java/com/example/lugares/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.lugares
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.lugares.databinding.ActivityMainBinding
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -21,5 +25,67 @@ class MainActivity : AppCompatActivity() {
         // Initializing firebase
         FirebaseApp.initializeApp(this)
         auth = Firebase.auth
+
+        // Assigning the methods to the buttons
+        binding.loginBtn.setOnClickListener{
+            login()
+        }
+
+        binding.registerBtn.setOnClickListener {
+            register()
+        }
+    }
+
+    private fun update(user: FirebaseUser?) {
+        if (user != null) {
+            val intent = Intent(this, PrincipalActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val user = auth.currentUser
+        update(user)
+    }
+
+    private fun login() {
+        val email = binding.editTextTextEmailAddress.text.toString()
+        val password = binding.editTextTextPassword.text.toString()
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){
+            task ->
+            if (task.isSuccessful) {
+                Log.d("Login user", "Success")
+                val user = auth.currentUser
+                Log.d("User", user.toString())
+                update(user)
+            } else {
+                Log.d("Login user", "Failed")
+                Toast.makeText(baseContext, "Fail", Toast.LENGTH_LONG).show()
+                update(null)
+            }
+        }
+    }
+
+    private fun register() {
+        val email = binding.editTextTextEmailAddress.text.toString()
+        val password = binding.editTextTextPassword.text.toString()
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this){
+                task ->
+            if (task.isSuccessful) {
+                Log.d("Creating user", "Registration")
+                val user = auth.currentUser
+                Log.d("User", user.toString())
+                update(user)
+            } else {
+                Log.d("Creating user", "Failed")
+                Toast.makeText(baseContext, "Fail", Toast.LENGTH_LONG).show()
+                update(null)
+            }
+        }
+
+
     }
 }
