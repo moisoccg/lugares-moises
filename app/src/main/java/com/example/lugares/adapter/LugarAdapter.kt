e37779135763ffbcdb9d35b29065e28046ed6c57
moisoccg <moisoccg@gmail.com>
2022-06-15 18:51:51 -0600

    missing files from previous commits

--- app/src/main/java/com/example/lugares/adapter/LugarAdapter.kt
+++ app/src/main/java/com/example/lugares/adapter/LugarAdapter.kt
@@ -1,4 +1,39 @@
 package com.example.lugares.adapter
 
-class LugarAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lugares.databinding.LugarFilaBinding
+import com.example.lugares.model.Lugar
+
+class LugarAdapter : RecyclerView.Adapter<LugarAdapter.LugarViewHolder>() {
+
+    //Lista para almacenar informacio
+    private var listaLugares = emptyList<Lugar>()
+
+    inner class LugarViewHolder(private val itemBinding: LugarFilaBinding) :
+            RecyclerView.ViewHolder(itemBinding.root){
+                fun bind(lugar: Lugar){
+                    itemBinding.lugarName.text = lugar.nombre
+                }
+            }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LugarAdapter.LugarViewHolder {
+       val itemBinding = LugarFilaBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+       return LugarViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: LugarViewHolder, position: Int) {
+        val lugarActual = listaLugares[position]
+        holder.bind(lugarActual)
+    }
+
+    override fun getItemCount(): Int {
+        return listaLugares.size
+    }
+
+    fun setData(lugares: List<Lugar>){
+        this.listaLugares = lugares
+        notifyDataSetChanged()
+    }
 }
--- app/src/main/java/com/example/lugares/ui/lugar/UpdateLugarFragment.kt
+++ app/src/main/java/com/example/lugares/ui/lugar/UpdateLugarFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.lugares.R
+import com.example.lugares.databinding.FragmentAddLugarBinding
+import com.example.lugares.databinding.FragmentUpdateLugarBinding
+import com.example.lugares.model.Lugar
+import com.example.lugares.viewmodel.LugarViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +24,40 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class UpdateLugarFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentUpdateLugarBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var lugarViewModel: LugarViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentUpdateLugarBinding.inflate(inflater,container,false)
+
+        lugarViewModel = ViewModelProvider(this).get(LugarViewModel::class.java)
+
+        binding.btnUpdate.setOnClickListener {
+            updateLugar()
+        }
+
+        return binding.root
+    }
+
+    private fun updateLugar(){
+        val nombre = binding.lugarName.text.toString()
+
+        if(validation(nombre)){
+            //guardar lugar
+            val lugar = Lugar(0,nombre)
+            lugarViewModel.updateLuggar(lugar) //enviando a la bd
+            Toast.makeText(requireContext(), getString(R.string.msg_success), Toast.LENGTH_LONG).show()
+        } else{
+            Toast.makeText(requireContext(), getString(R.string.msg_error), Toast.LENGTH_LONG).show()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update_lugar, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UpdateLugarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UpdateLugarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun validation(nombre: String): Boolean {
+        return !(nombre.isEmpty())
     }
 }
